8a7923dcb8dcadc013512518dfd7022cca39568c
Kevin Shanklin <williamkevinshanklin@gmail.com>
2019-08-20 16:42:59 -0400

    Updated script with TDC:RTDC mapping

--- Fastener_Script.py
+++ Fastener_Script.py
@@ -1,20 +1,28 @@
 import csv
 
 #Open csv Files
+
+rtdc_map = {}
+
 with open('Fasteners-By_TDC.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter = ',')
+	readCSV = csv.reader(csvfile, delimiter = ',')
 
-    header = next(readCSV)
-    index_by_cname = { key: index for index, key in enumerate(header) }
-    
+	header = next(readCSV)
+	index_by_cname = { key: index for index, key in enumerate(header) }
+
 #Reads Data in csv
-    for row in readCSV:
-    	tdc = row[index_by_cname["TDC"]]
-    	rtdc = row[index_by_cname["RTDC"]]
+	for row in readCSV:
+		tdc = row[index_by_cname["TDC"]]
+		rtdc = row[index_by_cname["RTDC"]]
 
-#Omits Blank TDC's
-    	if "" == tdc:
-    		continue
+	#Omits Blank TDC's
+		if "" == tdc:
+			continue
 
 #Prints TDC & UTDC
-    	print(tdc, rtdc)
+		print(tdc, rtdc)
+
+
+		rtdc_map[rtdc] = tdc
+
+print(rtdc_map)
