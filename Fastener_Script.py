8361d694d071ea2a738b87e95541d4b37d27385a
Kevin Shanklin <williamkevinshanklin@gmail.com>
2019-08-20 15:58:06 -0400

    Fastener Python Script
    Shows column "TDC" & "RTDC"

---
+++ Fastener_Script.py
@@ -0,0 +1,20 @@
+import csv
+
+#Open csv Files
+with open('Fasteners-By_TDC.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter = ',')
+
+    header = next(readCSV)
+    index_by_cname = { key: index for index, key in enumerate(header) }
+    
+#Reads Data in csv
+    for row in readCSV:
+    	tdc = row[index_by_cname["TDC"]]
+    	rtdc = row[index_by_cname["RTDC"]]
+
+#Omits Blank TDC's
+    	if "" == tdc:
+    		continue
+
+#Prints TDC & UTDC
+    	print(tdc, rtdc)
