f315bef20784e1ec99f251259a7337157e96cb25
Kevin Shanklin <williamkevinshanklin@gmail.com>
2019-08-27 09:54:30 -0400

    Added click user input for locating 'dups_folder" & ".csv location"

--- Fastener_Script.py
+++ Fastener_Script.py
@@ -3,6 +3,7 @@ import csv
 import os
 import sys
 import click
+from PyInquirer import prompt
 
 def build_rtdc_map(path_to_csv):
 	rtdc_map = {}
@@ -53,6 +54,20 @@ def rename_rtdc_files(path_to_root, rtdc_map, dups_folder):
 				print(f"ERROR: {rtdc} rename failed, skipping.")
 
 
+def select_dups_folder(message = "Please name your dups (duplicate) folder:"):
+	questions = [{'type': 'input','name': 'dups_folder','message': message}]
+	answers = prompt(questions)
+	if not answers:
+		raise Exception
+	return answers['dups_folder']
+
+def select_csv_file(message = "Please input your .csv location:"):
+	questions = [{'type': 'input','name': 'csv_folder','message': message}]
+	answers = prompt(questions)
+	if not answers:
+		raise Exception
+	return answers ['csv_folder']
+
 @click.command()
 @click.option('--dups_folder', '-df')
 @click.option('--path_to_fastener_photos', '-pfp')
@@ -62,8 +77,23 @@ def main(path_to_csv, path_to_fastener_photos, dups_folder):
 	This script renames "RTDCs" to "TDCs" of fastener photo data. First, 
 	locate your fastener photo data folder.
 	"""
+	if not dups_folder:
+		dups_folder = select_dups_folder()
 	if not os.path.exists(dups_folder): 
 		os.mkdir(dups_folder)
+
+	if not path_to_csv:
+		path_to_csv = select_csv_file()
+	if not os.path.exists(path_to_csv):
+		print (f'Hey. I can\'t seem to find a csv file called "{path_to_csv}"')
+		exit()
+	
+	if not path_to_fastener_photos:
+		path_to_fastener_photos = select_fastener_photos()
+	if not os.path.exists(path_to_fastener_photos):
+		print (f'Hey. I can\'t seem to find a file called "{path_to_fastener_photos}"')
+		exit()
+
 	rtdc_map = build_rtdc_map(path_to_csv)
 	rename_rtdc_files(path_to_fastener_photos, rtdc_map, dups_folder)
 
